49565ba600ee5218b25e8f1f9b22b87cda569148
abdulaziz-muhammed <abdulazizmuhammed49@gmail.com>
2026-06-29 22:22:36 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$db = new mysqli("sql210.infinityfree.com", "if0_42196289", "EZaVWI3X3DnBjJ", "if0_42196289_quickvtu");
+if ($db->connect_error) {
+    die("DB Error: " . $db->connect_error);
+}
+?>
